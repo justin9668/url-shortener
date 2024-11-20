61252b0b70192bde65328c114bc09d22e2f08eab
Justin Wang <justin1@bu.edu>
2024-11-20 05:07:46 -0500

    Update GitHub link in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
     return (
         <footer className="fixed bottom-4 w-full flex items-center justify-center">
             <a
-                href="https://github.com/justin9668/mp-5"
+                href="https://github.com/justin9668/url-shortener"
                 target="_blank"
             >
                 <Github className="w-6 h-6 hover:text-gray-600 transition-colors" />
